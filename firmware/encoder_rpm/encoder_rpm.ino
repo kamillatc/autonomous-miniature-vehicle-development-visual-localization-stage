1bbd051a6ede26ec11097e27e2244d15f048b784
Kamillla Costa <kamilla.txcosta@gmail.com>
2026-07-22 10:51:12 -0300

    Update encoder_rpm.ino

--- firmware/encoder_rpm/encoder_rpm.ino
+++ firmware/encoder_rpm/encoder_rpm.ino
@@ -12,12 +12,11 @@ rcl_allocator_t allocator;
 rcl_publisher_t pul;
 std_msgs__msg__Int32 msg_pul;
 
-// ===================== CONFIG =====================
 #define ENCODER_A 4
 #define ENCODER_B 19
 #define PULSOS_POR_VOLTA 1000
 #define JANELA_MS 10
-#define PUBLISH_MS 10   // 100 Hz de publicação
+#define PUBLISH_MS 10
 
 volatile uint32_t pulsos_A = 0;
 volatile uint32_t pulsos_B = 0;
@@ -27,7 +26,6 @@ volatile uint32_t pulsos_AS = 0;
 portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 SemaphoreHandle_t freqMutex;
 
-// ===================== INTERRUPÇÕES =====================
 void IRAM_ATTR contaPulsoA() {
   portENTER_CRITICAL_ISR(&mux);
   pulsos_A++;
@@ -40,7 +38,6 @@ void IRAM_ATTR contaPulsoB() {
   portEXIT_CRITICAL_ISR(&mux);
 }
 
-// ===================== TASK ENCODER (CORE 1) =====================
 void taskEncoder(void *pvParameters) {
   while (true) {
     vTaskDelay(pdMS_TO_TICKS(JANELA_MS));
@@ -53,7 +50,7 @@ void taskEncoder(void *pvParameters) {
       pulsos_B = 0;
     portEXIT_CRITICAL(&mux);
 
-      uint32_t janelas_por_minuto = 60000 / JANELA_MS; // ex: 6000 para JANELA_MS = 10
+      uint32_t janelas_por_minuto = 60000 / JANELA_MS;
 
       if(pulsos_AS < pulsos_BS){
         rpm = ((leitura * janelas_por_minuto) / PULSOS_POR_VOLTA) / 2;
@@ -61,11 +58,9 @@ void taskEncoder(void *pvParameters) {
       else{
         rpm = ((leitura * janelas_por_minuto) / PULSOS_POR_VOLTA) / 2;
       }
-//    vTaskDelay(1);
   }
 }
 
-// ===================== TASK MICRO-ROS (CORE 0) =====================
 void taskMicroROS(void *pvParameters) {
   TickType_t xLastWakeTime = xTaskGetTickCount();
   const TickType_t xPeriod = pdMS_TO_TICKS(PUBLISH_MS);
@@ -76,7 +71,6 @@ void taskMicroROS(void *pvParameters) {
   }
 }
 
-// ===================== SETUP =====================
 void setup() {
   Serial.begin(115200);
   Serial2.begin(115200, SERIAL_8N1, 16, 17);
@@ -118,7 +112,6 @@ void setup() {
   );
 }
 
-// ===================== LOOP =====================
 void loop() {
   vTaskDelay(pdMS_TO_TICKS(1000));
 }
